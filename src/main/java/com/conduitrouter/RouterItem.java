c044f3a0a4798e7c834df85b0cccb10559c8531a
digestlotion <keithersuars@gmail.com>
2026-05-14 20:30:56 +0800

    tooltip

--- src/main/java/com/conduitrouter/RouterItem.java
+++ src/main/java/com/conduitrouter/RouterItem.java
@@ -7,6 +7,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
+import javax.annotation.Nullable;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
@@ -16,6 +17,7 @@ import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.NbtUtils;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
@@ -23,6 +25,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -267,5 +270,17 @@ public class RouterItem extends Item {
 
         bufferSource.endBatch(RenderType.cutoutMipped());
         poseStack.popPose();
+    }
+
+    @Override
+    public void appendHoverText(
+        ItemStack stack,
+        @Nullable Level level,
+        List<Component> tooltipComponents,
+        TooltipFlag isAdvanced
+    ) {
+        tooltipComponents.add(Component.literal("§7Hold pipe in your offhand"));
+        tooltipComponents.add(Component.literal("§8Right-click: place point"));
+        tooltipComponents.add(Component.literal("§8Right-click again: confirm points"));
     }
 }
